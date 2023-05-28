8c944c4d68edc8c97d0e642d0348eca3353f96a6
Kiruel <etiennetheodore@orange.fr>
2023-05-28 16:54:02 +0200

    fet: add extension list

--- lib/api_client.dart
+++ lib/api_client.dart
@@ -1,4 +1,5 @@
 import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
 import 'package:json_annotation/json_annotation.dart';
 import 'package:retrofit/retrofit.dart';
 
---
+++ lib/list_view_refreshable.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/widgets.dart';
+import 'package:pull_to_refresh/pull_to_refresh.dart';
+
+extension ListViewRefreshable on ListView {
+  Widget refreshable(Future<void> Function() onRefresh) => RefreshableWidget(
+        builder: (refreshController) => SmartRefresher(
+          controller: refreshController,
+          onRefresh: () async {
+            await onRefresh.call();
+            refreshController.refreshCompleted();
+          },
+          child: this,
+        ),
+      );
+}
+
+class RefreshableWidget extends StatefulWidget {
+  const RefreshableWidget({super.key, required this.builder});
+  final Widget Function(RefreshController refreshController) builder;
+  @override
+  State<RefreshableWidget> createState() => _RefreshableWidgetState();
+}
+
+class _RefreshableWidgetState extends State<RefreshableWidget> {
+  final RefreshController refreshController = RefreshController();
+
+  @override
+  Widget build(BuildContext context) {
+    return widget.builder(refreshController);
+  }
+}
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView().refreshable(() async {
+      await Future.delayed(Duration(milliseconds: 1000));
+      print('toto');
+    });
+  }
+}
